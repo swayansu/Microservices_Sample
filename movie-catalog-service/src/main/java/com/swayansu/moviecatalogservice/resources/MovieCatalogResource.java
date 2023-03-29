2b8617a47e3beda13fd8ad4a939b01acff31cf53
Swayansu <satyaprajna1313@gmail.com>
2023-03-29 16:20:20 +0530

    the updated API calls

--- movie-catalog-service/src/main/java/com/swayansu/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/swayansu/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.swayansu.moviecatalogservice.resources;
 import com.swayansu.moviecatalogservice.models.CatalogItem;
 import com.swayansu.moviecatalogservice.models.Movie;
 import com.swayansu.moviecatalogservice.models.Rating;
+import com.swayansu.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -32,41 +34,32 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
 
-        // RestTemplate the class which helps in getting the classes
-
         // TODO: get all the movie IDs
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
 
-        // TODO: USING WEBCLIENT
-        return ratings.stream().map(rating -> {
-                  Movie movie = webClientBuilder.build()
-                           .get()
-                           .uri("http://localhost:8082/movies/" + rating.getMovieId())
-                           .retrieve()
-                           .bodyToMono(Movie.class)
-                           .block();
-                   return new CatalogItem(movie.getMovieName(), "Harry potter sequqel", 4);
-               })
+
+        // TODO: Using restTemplate
+        return ratings.getUserRating().stream().map(
+                // TODO: For each Movie Id, call movie info service and get details
+                rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+
+                    // TODO: Put them all together
+                    return new CatalogItem(movie.getMovieName(), "Harry potetr sequqel", rating.getRating());
+                }
+        )
                 .collect(Collectors.toList());
-
-//        return ratings.stream().map(
-//                rating -> {
-//                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-//                   return new CatalogItem(
-//                            movie.getMovieName(),
-//                            "Harry potetr sequqel",
-//                            4
-//                    );
-//                }
-//        )
-//                .collect(Collectors.toList());
-
-        // TODO: For each Movie Id, call movie info service and get details
-
-        // TODO: Put them all together
 
     }
 }
+// TODO: USING WEBCLIENT
+//        return ratings.stream().map(rating -> {
+//                  Movie movie = webClientBuilder.build()
+//                           .get()
+//                           .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                           .retrieve()
+//                           .bodyToMono(Movie.class)
+//                           .block();
+//                   return new CatalogItem(movie.getMovieName(), "Harry potter sequqel", 4);
+//               })
+//                .collect(Collectors.toList());
--- ratings-data-service/src/main/java/com/swayansu/ratingsdataservice/resources/RatingResources.java
+++ ratings-data-service/src/main/java/com/swayansu/ratingsdataservice/resources/RatingResources.java
@@ -1,16 +1,32 @@
 package com.swayansu.ratingsdataservice.resources;
 
 import com.swayansu.ratingsdataservice.models.Rating;
+import com.swayansu.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingResources {
 
-    @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId){
-        return  new Rating(movieId, 4);
+//    @RequestMapping("/{movieId}")
+//    public Rating getRating(@PathVariable("movieId") String movieId){
+//        return  new Rating(movieId, 4);
+//    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId ){
+        List<Rating> ratingList = Arrays.asList(
+                new Rating("1234", 5),
+                new Rating("5678", 4),
+                new Rating("7890", 4)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratingList);
+        return userRating;
     }
 }
